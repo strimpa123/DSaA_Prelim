cd71007405c3ddf7080a3cebbbc69ea2505edacb
Kian_Laptop_HP_ELITEBOOK <Kian@DESKTOP-U0E91TN>
2025-03-25 06:18:53 +0800

    modified notifications

--- DSaA_Prelim/Program.cs
+++ DSaA_Prelim/Program.cs
@@ -13,7 +13,7 @@ namespace DSaA_Prelim
         static void Main(string[] args)
         {
             int[][] tower = new int[3][];
-            ConsoleColor[] colors = {ConsoleColor.White,ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.DarkRed,ConsoleColor.DarkBlue, ConsoleColor.DarkGreen, ConsoleColor.DarkYellow,ConsoleColor.DarkMagenta };
+            ConsoleColor[] colors = {ConsoleColor.White,ConsoleColor.DarkGreen, ConsoleColor.DarkCyan, ConsoleColor.Cyan, ConsoleColor.DarkMagenta, ConsoleColor.Yellow, ConsoleColor.DarkYellow,ConsoleColor.Blue, ConsoleColor.Gray, ConsoleColor.Red,ConsoleColor.Green };
             int diskCount = 0;
             int moveCount = 0;
             bool userDisk = false;
@@ -25,10 +25,8 @@ namespace DSaA_Prelim
                 if (int.TryParse(Console.ReadLine(), out diskCount)) {
                     if (diskCount > 10)
                         Console.WriteLine("Disks should be 10 AT MOST");
-
                     else if (diskCount < 3)
                         Console.WriteLine("Disks should be 3 AT LEAST");
-
                     else
                         userDisk = true;
                 }
@@ -51,24 +49,62 @@ namespace DSaA_Prelim
                 }
             }
 
-            for (int i = 0; i < (diskCount + 2); i++)  {             // This is the Printer Loop for elements of the Tower.
-                for (int j = 0; j < tower.Length; j++) {
+            Console.WriteLine($"Move Counter: {moveCount}");        // This is the Printer Loop for elements of the Tower.
+            for (int i = 0; i < (diskCount + 2); i++)  {
+                for (int j = 0; j < tower.Length; j++)  {
                     Console.ForegroundColor = colors[tower[j][i]];
+                    if (j == 0)
+                        Console.Write("\t");
                     if (tower[j][i] != 0)
                         Console.Write($"[{tower[j][i]}]\t");
                     else
                         Console.Write(" |\t");
                     Console.ResetColor();
                 }
-                Console.WriteLine();
+                Console.WriteLine("");
                 if (i+1 == (diskCount + 2))
-                    Console.WriteLine($"-0-\t-1-\t-2-");
+                    Console.WriteLine($"\t-0-\t-1-\t-2-");
             }
             Console.WriteLine($"Welcome to Tower of Hanoi!\nThe Goal is to move all the disk to Tower [2].\n|Reminder|\nYou can't put higher value disk on top of lower value disk.\n\nPress enter to Start...");
             Console.ReadKey();
+
             while (mainGame) // This Holds the whole main loop for the whole game
             {
                 bool towerAuth = false;
+                Console.Clear();
+                Console.WriteLine($"Move Counter: {moveCount}"); // Displays the main User Interface
+                for (int i = 0; i < (diskCount + 2); i++) {
+                    for (int j = 0; j < tower.Length; j++) {
+                        Console.ForegroundColor = colors[tower[j][i]];
+                        if (j == 0)
+                            Console.Write("\t");
+                        if (tower[j][i] != 0)
+                            Console.Write($"[{tower[j][i]}]\t");
+                        else
+                            Console.Write(" |\t");
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine("");
+                    if (i + 1 == (diskCount + 2))
+                        Console.WriteLine($"\t-0-\t-1-\t-2-");
+                }
+
+                for (int k = 0; k < tower[0].Length - 2; k++) { // This checks for winning Condition
+                    if (tower[2][tower[2].Length - 1 - k] != tower[2].Length - 2 - k)
+                        break;
+                    else if (k >= tower.Length - 1) {
+                        int leastMoves = 2;
+                        for (int i = 0; i < diskCount - 1; i++) // obtains the least/most efficient moves required and compare it to the user move counter
+                            leastMoves = leastMoves * 2;
+                        leastMoves = leastMoves - 1;
+                        float accuracy = ((float)leastMoves / (float)moveCount) * 100;
+                        Console.WriteLine($"Congratulations! You have completed the tower! | [{moveCount}] of [{leastMoves}] | Your Accuracy is [{accuracy}%]!");
+                        Console.ReadKey();
+                        mainGame = false;
+                        towerAuth = true;
+                        break;
+                    }
+                }
 
                 while (!towerAuth)  // This holds the authentication for tower choices of the user and under goes several constraints
                 {
@@ -82,7 +118,7 @@ namespace DSaA_Prelim
                     int targetIndex = 0;
                     do
                     {
-                        Console.Write($"\nSource Tower: ");
+                        Console.Write($"\nFrom which tower will the disk be coming from? Only input 0,1 or 2: ");   // This is the source tower section
                         string userSorT = Console.ReadLine();
                         int userTower;
                         if (int.TryParse(userSorT, out userTower)) {
@@ -102,7 +138,7 @@ namespace DSaA_Prelim
                                     Console.WriteLine("No disk in this tower");
                             }
                             else
-                                Console.WriteLine("Enter [0] [1] [2] Tower Only");
+                                Console.WriteLine("You have failed to identify which tower the disk will come from! Try Again!");
                         }
                         else
                             Console.WriteLine("Enter a valid Tower");
@@ -112,12 +148,12 @@ namespace DSaA_Prelim
                     inputAuth = false;
                     do
                     {
-                        Console.Write($"\nTarget Tower: ");
+                        Console.Write($"\nTo which tower will the disk be going to? Only input 0,1 or 2: ");    // This is the section for the designation tower
                         string userTarT = Console.ReadLine();
                         int userTower;
                         if (int.TryParse(userTarT, out userTower)) {
                             if (userTower == sourceTower)
-                                Console.WriteLine("You cant use the same tower");
+                                Console.WriteLine("The designated towers should not be the same! Please try again!");
                             else if (userTower <= 2 && userTower >= 0) {
                                 targetTower = userTower;
                                 inputAuth = true;
@@ -134,13 +170,12 @@ namespace DSaA_Prelim
                                     targetIndex = tower[targetTower].Length - 1;
                             }
                             else
-                                Console.WriteLine("Enter [0] [1] [2] Tower Only");
+                                Console.WriteLine("You have failed to identify which tower the disk will be going to! Try Again!");
                         }
                         else
                             Console.WriteLine("Enter a valid Tower");
-
                     } while (!inputAuth);
-                    Console.Clear();
+
                     if (targetDisk > sourceDisk) {        // This section is responsible for updating the elements of the towers.
                         tower[targetTower][targetIndex - 1] = sourceDisk;
                         tower[sourceTower][sourceIndex] = 0;
@@ -155,37 +190,6 @@ namespace DSaA_Prelim
                     }
                     else
                         Console.WriteLine("The disk thats is going to move has a higher value than the existing disk in that tower.");
-
-                    Console.WriteLine($"Move Counter: {moveCount}"); // Displays the main User Interface
-                    for (int i = 0; i < (diskCount + 2); i++) {
-                        for (int j = 0; j < tower.Length; j++) {
-                            Console.ForegroundColor = colors[tower[j][i]];
-                            if (tower[j][i] != 0)
-                                Console.Write($"[{tower[j][i]}]\t");
-                            else
-                                Console.Write(" |\t");
-                        }
-                        Console.WriteLine();
-                        Console.ResetColor();
-                        if (i + 1 == (diskCount + 2))
-                            Console.WriteLine($"-0-\t-1-\t-2-");
-                    }
-                    for (int k = 0; k < tower[0].Length-2; k++) { // This checks for winning Condition
-                        if (tower[2][tower[2].Length-1-k] != tower[2].Length-2-k)
-                            break;
-
-                        else if (k >= tower.Length - 1) {
-                            int leastMoves = 2;
-                            for (int i = 0; i < diskCount-1; i++) // obtains the least/most efficient moves required and compare it to the user move counter
-                                leastMoves = leastMoves * 2;
-                            leastMoves = leastMoves-1;
-                            float accuracy = ((float)leastMoves / (float)moveCount)*100;
-                            Console.WriteLine($"FINISHED!\nYour Accuracy is [{accuracy}%] | [{moveCount}] of [{leastMoves}]");
-                            Console.ReadKey();
-                            mainGame = false;
-                            break;
-                        }
-                    }
                 }
             }
         }
